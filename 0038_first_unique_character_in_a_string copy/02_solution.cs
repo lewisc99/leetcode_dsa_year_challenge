0a8b2d434dd8c5f6ba27564f61fe109b01fdbda1
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-02-17 22:02:35 -0300

    0038_first_unique_character_in_a_string copy/02_solution.cs

---
+++ 0038_first_unique_character_in_a_string copy/02_solution.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+  public string ClearDigits(string s)
+  {
+    Stack<char> stack = new Stack<char>();
+
+    foreach (char c in s)
+    {
+      if (char.IsDigit(c))
+      {
+        if (stack.Count > 0)
+          stack.Pop();
+      }
+      else
+      {
+        stack.Push(c);
+      }
+    }
+
+    return new string(stack.Reverse().ToArray());
+  }
+}
